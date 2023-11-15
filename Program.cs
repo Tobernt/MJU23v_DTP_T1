77e66d8346159ddc8301c17e4eb32acd201bd8fa
Tobernt <tobias_berntsson@hotmail.com>
2023-11-15 12:18:01 +0100

    Added Comment for order of implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,37 @@
-﻿using System.Globalization;
+﻿/* 
+DELUPPGIFT 1: Ladda filen lang.txt vid programstart
+1.1 Implementera kod för att läsa innehållet från lang.txt (Redan gjort av läraren)
+1.2 Testa och säkerställ att filen laddas korrekt  (körde en test körning och den listar infon)
+
+DELUPPGIFT 2: Kommandoradsloop och Basfunktionalitet
+2.1 Implementera en enkel kommandoradsloop
+2.2 Skapa kommandon för list group, list country, list between, show language, show group,show country countryname, show between lownum and hinum och population group groupname
+2.3 Skapa help och quit kommandon
+2.4 Gör stage/commit/push för varje implementerat kommando
+
+DELUPPGIFT 3: Implementera Fyra Frågekommandon
+3.1 Implementera kod för varje kommando i 2.2
+3.2 Gör stage/commit/push för varje implementerat kommando
+
+DELUPPGIFT 4: Hjälputskrift och Avsluta Kommandon
+4.1 Implementera help kommandot för att visa tillgängliga kommandon
+4.2 Implementera quit kommandot för att avsluta programmet
+4.3 Gör stage/commit/push för varje implementerat kommando
+
+DELUPPGIFT 5: Kommentera Kod
+5.1 Kommentera koden för förståelse och läsbarhet
+5.2 Lägg till //NYI-kommentarer för de kommandon som inte har implementerats
+5.3 Lägg till //TBD-funktioner om det finns möjlighet till refaktorering i framtiden
+5.4 Gör stage/commit/push för detta steg
+
+DELUPPGIFT 6: Validering och Testning
+6.1 Testa varje kommando och se till att de ger förväntade resultat
+6.2 Validera användarens inmatning och hantera felaktiga kommandon med //FIXME-kommentarer
+6.3 Gör stage/commit/push för detta steg
+*/
+
+
+using System.Globalization;
 
 namespace MJU23v_DTP_T1
 {
